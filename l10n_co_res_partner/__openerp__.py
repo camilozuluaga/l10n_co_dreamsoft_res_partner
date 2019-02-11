6b7b4d40c151f682c0cac23a32f495c768ebe979
camilozuluaga <juancamilozuluagaserna@hotmail.com>
2019-02-11 16:07:49 -0500

    agregando nuevo modulo

---
+++ l10n_co_res_partner/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Colombia - Terceros',
+    'category': 'Localization',
+    'version': '10.01',
+    'author': 'Dominic Krimmer, Plastinorte S.A.S, Odoo LoCo',
+    'license': 'AGPL-3',
+    'maintainer': 'dominic.krimmer@gmail.com',
+    'website': 'https://www.plastinorte.com',
+    'summary': 'Colombia Terceros: Extended Partner / '
+               'Contact Module - Odoo 10.0',
+    'images': ['images/main_screenshot.png'],
+    'depends': [
+        'account',
+        'account_accountant',
+        'base'
+    ],
+    'data': [],
+    'installable': True,
+}
